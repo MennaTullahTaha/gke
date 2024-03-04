43c5fdea7ef820a2cfc4e272c91a2ee656bacb59
Menna Tullah Magdy Taha <menna.tullah.magdy731@gmail.com>
2024-03-04 13:58:53 +0100

    Renamed the variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
-variable "gcp-credentials" {
+variable "gcp_credentials" {
   type        = string
   description = "Location of service account for GCP"
 }
 
-variable "gcp-project-id" {
+variable "gcp_project_id" {
   type        = string
   description = "GCP project id."
 }
 
-variable "gcp-region" {
+variable "gcp_region" {
   type        = string
   description = "GCP region"
 }
