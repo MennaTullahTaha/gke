2e478e61d304c45c33d05bfbb1f775716c73a0f0
Menna Tullah Magdy Taha <menna.tullah.magdy731@gmail.com>
2024-03-04 15:54:00 +0100

    edited modules in main

--- providers.tf
+++ providers.tf
@@ -3,3 +3,11 @@ provider "google" {
   project = var.gcp_project_id
   region = var.gcp_region
 }
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
